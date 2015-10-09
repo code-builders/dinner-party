a7aa6b077d40e2605287526d2f9c9a2c93859334
Karen Hambro <khambro@gmail.com>
2015-10-09 14:05:37 -0700

    Initial dinner party commit

---
+++ Karen_dinner_party.rb
@@ -0,0 +1,6 @@
+
+
+party_elements = {beverages: ["coke", "diet coke", "sprite", "tequila", "tequiza", "zima", "bud light lime", "orange juice", "water", "coffee", "tea", "jim beam", "cerveza"], food: ["porkchop", "veggie nuggets", "shrimp", "spaghetti"], people:
+["Karen", "Wolfie", "Erin", "Tim", "Thom", "Stephen", "Ken", "James"]}
+
+puts "#{party_elements[:people].sample} will have the #{party_elements[:food].sample} to eat with #{party_elements[:food].sample} on the side. And to drink, #{party_elements[:beverages].sample}! Yum!"
