15238a5bdf685c45f2b8a24c2e21048cf268874e
smurfik <smurfik@mlpinit.com>
2015-10-09 14:36:04 -0700

    add while loop

--- tammy_dinner-party.rb
+++ tammy_dinner-party.rb
@@ -1,7 +1,17 @@
-beverages = ["tea", "coffee", "lemonade"]
+drinks = ["tea", "coffee", "lemonade", "water"]
 
-food = ["cookies", "cake", "ice cream"]
+desserts = ["cookies", "cake", "ice cream", "candy"]
 
-people = ["Lily", "Bob", "Mark"]
+people = ["Lily", "Bob", "Mark", "Frank"]
 
-puts "#{people.pop} is eating #{food.pop} and drinking #{beverages.pop}."
+seat_number = [1, 3, 5, 7]
+
+while drinks != []
+  person = {name: people.pop,
+            beverage: drinks.pop,
+            food: desserts.pop,
+            seat_number: seat_number.pop 
+  }
+
+  puts person
+end
