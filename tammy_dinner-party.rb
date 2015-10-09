19367798cabf086314daf479b9d052d7ba2733b3
smurfik <smurfik@mlpinit.com>
2015-10-09 13:57:29 -0700

    add initial file

---
+++ tammy_dinner-party.rb
@@ -0,0 +1,7 @@
+beverages = ["tea", "coffee", "lemonade"]
+
+food = ["cookies", "cake", "ice cream"]
+
+people = ["Lily", "Bob", "Mark"]
+
+puts "#{people.pop} is eating #{food.pop} and drinking #{beverages.pop}."
