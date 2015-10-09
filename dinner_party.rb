1fcab0eab618e0b64737083deaac5456d1298d40
Chris Hintz <chrishintz42@gmail.com>
2015-10-09 15:34:39 -0700

    Make dinner file with hash added

--- dinner_party.rb
+++ dinner_party.rb
@@ -1,12 +1,16 @@
-beverage =["Soda", "Wine", "Beer", "Champagne", "Vodka", "Water", "Juice", "Milk", "Coffee", "Tea"]
-food = ["Pizza", "Veggies", "Steak", "Chicken", "Fish", "Pork", "Baked Potato", "Chow Mein", "Hot Dog", "Philly Cheesesteak"]
-people = ["Bookis", "Elise", "Daniel", "LeShoya", "Evelyn", "Karen", "Tammy", "Chris", "Calla", "Pawpea"].shuffle
-seating_position = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Chris Hintz
+
+dp = {
+beverage: ["Soda", "Wine", "Beer", "Champagne", "Vodka", "Water", "Juice", "Milk", "Coffee", "Tea"],
+dinner: ["Pizza", "Veggies", "Steak", "Chicken", "Fish", "Pork", "Baked Potato", "Chow Mein", "Hot Dog", "Philly Cheesesteak"],
+people: ["Bookis", "Elise", "Daniel", "LeShoya", "Evelyn", "Karen", "Tammy", "Chris", "Calla", "Pawpea"].shuffle,
+seating_position: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+}
 
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |number|
-  puts "Seating position: " + seating_position.pop.to_s
-  puts "Person: " + people.pop
-  puts "Your dinner: " + food.sample
-  puts "Your beverage: " + beverage.sample
+  puts "Seating position: " + dp[:seating_position].pop.to_s
+  puts "Person: " + dp[:people].pop
+  puts "Your dinner: " + dp[:dinner].sample
+  puts "Your beverage: " + dp[:beverage].sample
   puts ""
 end
