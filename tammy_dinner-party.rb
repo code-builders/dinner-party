83889f8b80db76af3dc5f6837949f53c9ea5d1d3
smurfik <smurfik@mlpinit.com>
2015-10-09 15:19:52 -0700

    add puts statement

--- tammy_dinner-party.rb
+++ tammy_dinner-party.rb
@@ -1,16 +1,16 @@
 puts "What is your name?"
 
-answer = gets.chomp
+answer = gets.chomp.capitalize
 people = [answer]
 
 puts "What kind of drink would you like?"
 
-answer = gets.chomp
+answer = gets.chomp.downcase
 drinks = [answer]
 
 puts "What kind of dessert would you like?"
 
-answer = gets.chomp
+answer = gets.chomp.downcase
 desserts = [answer]
 
 seat_number = [1, 3, 5, 7]
@@ -22,5 +22,5 @@ while drinks != []
             seat_number: seat_number.pop
   }
 
-  puts person
+  puts "Hi #{person[:name]}! Here is #{person[:beverage]} and #{person[:food]}. Your seat number is #{person[:seat_number]}. ENJOY!"
 end
