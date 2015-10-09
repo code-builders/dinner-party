d5a04a21f1939b1e4fc093e022df3ba5adbffddb
Karen Hambro <khambro@gmail.com>
2015-10-09 14:24:25 -0700

    Initial dinner party list

--- Karen_dinner_party.rb
+++ Karen_dinner_party.rb
@@ -3,4 +3,9 @@
 party_elements = {beverages: ["coke", "diet coke", "sprite", "tequila", "tequiza", "zima", "bud light lime", "orange juice", "water", "coffee", "tea", "jim beam", "cerveza"], food: ["porkchop", "veggie nuggets", "shrimp", "spaghetti"], people:
 ["Karen", "Wolfie", "Erin", "Tim", "Thom", "Stephen", "Ken", "James"]}
 
-puts "#{party_elements[:people].sample} will have the #{party_elements[:food].sample} to eat with #{party_elements[:food].sample} on the side. And to drink, #{party_elements[:beverages].sample}! Yum!"
+#puts "#{party_elements[:people].sample} will have the #{party_elements[:food].sample} to eat with #{party_elements[:food].sample} on the side. And to drink, #{party_elements[:beverages].sample}! Yum!"
+
+
+party_elements[:people].each do |person|
+  puts "#{person} will have the #{party_elements[:food].sample} to eat with #{party_elements[:food].sample} on the side. And to drink, #{party_elements[:beverages].sample}! Yum!"
+end
