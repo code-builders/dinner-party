3d6882e40ecb6dd285523a18eb5e6e1187192ddb
Karen Hambro <khambro@gmail.com>
2015-10-11 11:45:50 -0700

    fixed things

--- Karen_dinner_party.rb
+++ Karen_dinner_party.rb
@@ -1,7 +1,11 @@
 
+#use for a pre-populated party with guest, food, and bevs. Otherwise use user input option below.
+#party_elements = {beverages: ["coke", "diet coke", "sprite", "tequila", "tequiza", "zima", "bud light lime", "orange juice", "water", "coffee", "tea", "jim beam", "cerveza"], food: ["porkchop", "veggie nuggets", "shrimp", "spaghetti"], people:
+#["Karen", "Wolfie", "Erin", "Tim", "Thom", "Stephen", "Ken", "James"], seat_num:[]}
 
-party_elements = {beverages: ["coke", "diet coke", "sprite", "tequila", "tequiza", "zima", "bud light lime", "orange juice", "water", "coffee", "tea", "jim beam", "cerveza"], food: ["porkchop", "veggie nuggets", "shrimp", "spaghetti"], people:
-["Karen", "Wolfie", "Erin", "Tim", "Thom", "Stephen", "Ken", "James"], seat_num:[]}
+#user input arrays
+party_elements = {beverages: [], food: [], people:
+[], seat_num:[]}
 
 
 #puts "#{party_elements[:people].sample} will have the #{party_elements[:food].sample} to eat with #{party_elements[:food].sample} on the side. And to drink, #{party_elements[:beverages].sample}! Yum!"
@@ -16,6 +20,7 @@ puts "Who would you like to invite to the party?"
     array.push invite
   end
   array.pop
+  puts "Your guests are:"
   puts array
 
 puts "What dishes would you like to serve?"
@@ -28,6 +33,7 @@ puts "What dishes would you like to serve?"
     array.push dishes
   end
   array.pop
+  puts "Your dishes are:"
   puts array
 
 puts "What beverages would you like to serve?"
@@ -40,6 +46,7 @@ puts "What beverages would you like to serve?"
     array.push drinks
   end
   array.pop
+  puts "Your beverages are:"
   puts array
 
 
@@ -49,9 +56,9 @@ puts "What beverages would you like to serve?"
   #seat range
   party_elements[:seat_num]= 1..sit
   party_elements[:seat_num]= party_elements[:seat_num].to_a
-  puts party_elements[:seat_num]
+  #puts party_elements[:seat_num]
 
-
+puts "Your party lineup is the following:"
 
 party_elements[:people].each do |person|
   puts "#{person} (seat #{party_elements[:seat_num].shuffle!.pop}) will have the #{party_elements[:food].sample} to eat with #{party_elements[:food].sample} on the side. And to drink, #{party_elements[:beverages].sample}! Yum!"
