b2fada4ab4c5de90120b6fd3c4d875272b1d9f6c
Evelyn <evieb16@gmail.com>
2015-10-10 23:28:43 -0700

    Added user input functions

--- dinner_party.rb
+++ dinner_party.rb
@@ -1,32 +1,68 @@
 Dinner_party = {
 
-  beverage: ["beer", "wine", "water"],
-  food: ["chicken", "steak", "veggie burger"],
-  people: ["Mom", "Dad", "Brother", "Sister"],
-  people2: ["Mom", "Dad", "Brother", "Sister"],
-  seat_num: [1, 2, 3, 4]
+  beverages: [],
+  food: [],
+  guests: []
 
 }
 
-puts "The following people are coming to my dinner party: #{Dinner_party[:people].shuffle!.pop},
-#{Dinner_party[:people].shuffle!.pop}, #{Dinner_party[:people].shuffle!.pop}, and #{Dinner_party[:people].shuffle!.pop}."
+#get guest names
+puts "Enter the names of the guests to your dinner party. Type \"done\" when finished."
+guests = ""
+while guests != "done"
+  guests = gets.chomp
+  if guests != "done"
+    Dinner_party[:guests].push(guests)
+  end
+end
 
 puts ""
 
-puts "#{Dinner_party[:people2].shuffle!.pop} will be having the #{Dinner_party[:food].sample} with #{Dinner_party[:beverage].sample}
-and will be sitting in Seat #{Dinner_party[:seat_num].shuffle!.pop}."
+puts "The following are coming to your dinner party:"
+puts Dinner_party[:guests]
 
 puts ""
 
-puts "#{Dinner_party[:people2].shuffle!.pop} will be having the #{Dinner_party[:food].sample} with #{Dinner_party[:beverage].sample}
-and will be sitting in Seat #{Dinner_party[:seat_num].shuffle!.pop}."
+#get drink options
+puts "Enter the beverages you will serve at your dinner party. Type \"done\" when finished."
+beverages = ""
+while beverages != "done"
+  beverages = gets.chomp
+  if beverages != "done"
+    Dinner_party[:beverages].push(beverages)
+  end
+end
 
 puts ""
 
-puts "#{Dinner_party[:people2].shuffle!.pop} will be having the #{Dinner_party[:food].sample} with #{Dinner_party[:beverage].sample}
-and will be sitting in Seat #{Dinner_party[:seat_num].shuffle!.pop}."
+puts "The following beverages will be served at your dinner party:"
+puts Dinner_party[:beverages]
 
 puts ""
 
-puts "#{Dinner_party[:people2].shuffle!.pop} will be having the #{Dinner_party[:food].sample} with #{Dinner_party[:beverage].sample}
-and will be sitting in Seat #{Dinner_party[:seat_num].shuffle!.pop}."
+#get food options
+puts "Enter the dishes you will serve at your dinner party. Type \"done\" when finished."
+food = ""
+while food != "done"
+  food = gets.chomp
+  if food != "done"
+    Dinner_party[:food].push(food)
+  end
+end
+
+puts ""
+
+puts "The following dishes will be served at your dinner party:"
+puts Dinner_party[:food]
+
+puts ""
+
+puts "Type \"generate\" to see the results!"
+if gets.chomp == "generate"
+#assign each guest a random dish and beverage with seat number
+  seat_num = 0
+  Dinner_party[:guests].shuffle.each do |guest|
+    seat_num = seat_num + 1
+    puts "#{guest} will be having the #{Dinner_party[:food].sample} dish with #{Dinner_party[:beverages].sample} to drink. #{guest} will be sitting in seat #{seat_num}."
+  end
+end
